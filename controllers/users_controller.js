e2e89694678addad0459a50525b5305bf2080f9c
Subhankr12 <subhankr12@gmail.com>
2020-07-26 18:50:50 +0530

    create model for user and setup sign up and sign in page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,54 @@
+const User = require("../models/user");
+
+//render profile page
 module.exports.profile = (req, res) => {
   return res.render("user_profile", {
     title: "User profile",
   });
+};
+
+//render the sign up page
+module.exports.signUp = (req, res) => {
+  return res.render("user_sign_up", {
+    title: "Codeial | Sign Up",
+  });
+};
+
+//render the sign in page
+module.exports.signIn = (req, res) => {
+  return res.render("user_sign_in", {
+    title: "Codeial | Sign In",
+  });
+};
+
+//get the sign up data
+module.exports.create = (req, res) => {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
+
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) {
+      console.log("Error in finding user in signing up!");
+      return;
+    }
+
+    if (!user) {
+      User.create(req.body, (err, user) => {
+        if (err) {
+          console.log("Error in creating user while signing up");
+          return;
+        }
+
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
+
+//sign in and create session for the user
+module.exports.createSession = (req, res) => {
+  //TODO later
 };
