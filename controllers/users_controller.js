881d3af52f7bd05f25c9c1fcae939bc468a4f47d
Subhankr12 <subhankr12@gmail.com>
2020-07-26 21:05:25 +0530

    set manula local authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,9 +2,20 @@ const User = require("../models/user");
 
 //render profile page
 module.exports.profile = (req, res) => {
-  return res.render("user_profile", {
-    title: "User profile",
-  });
+  if (req.cookies.user_id) {
+    User.findById(req.cookies.user_id, (err, user) => {
+      if (user) {
+        return res.render("user_profile", {
+          title: "User profile",
+          user: user,
+        });
+      } else {
+        return res.redirect("/users/sign-in");
+      }
+    });
+  } else {
+    return res.redirect("/users/sign-in");
+  }
 };
 
 //render the sign up page
@@ -50,5 +61,27 @@ module.exports.create = (req, res) => {
 
 //sign in and create session for the user
 module.exports.createSession = (req, res) => {
-  //TODO later
+  //Steps to authenticate
+  //find the user
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) {
+      console.log("Error in finding user in signing in");
+      return;
+    }
+
+    //handle user found
+    if (user) {
+      // handle password which doesn't match
+      if (user.password != req.body.password) {
+        return res.redirect("back");
+      }
+
+      //handle session creation
+      res.cookie("user_id", user.id);
+      return res.redirect("/users/profile");
+    } else {
+      //handle user not found
+      return res.redirect("back");
+    }
+  });
 };
