a8dc47612fabd06819188a0153e0ec770024a851
Subhankr12 <subhankr12@gmail.com>
2020-07-28 16:27:33 +0530

    show list of posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,23 @@
+const Post = require("../models/post");
+
 module.exports.home = (req, res) => {
   // console.log(req.cookies);
   // res.cookie("user_id", 25);
-  return res.render("home", {
-    title: "Home",
-  });
+
+  // Post.find({}, (err, posts) => {
+  //   return res.render("home", {
+  //     title: "Codeial | Home",
+  //     posts: posts,
+  //   });
+  // });
+
+  //populate the user of each post
+  Post.find({})
+    .populate("user")
+    .exec((err, posts) => {
+      return res.render("home", {
+        title: "Codeial | Home",
+        posts: posts,
+      });
+    });
 };
