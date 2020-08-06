70f6576975e3b6de340db6f1a18cef1923c6781e
Subhankr12 <subhankr12@gmail.com>
2020-08-06 14:47:21 +0530

    add profile upload option using multer

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const fs = require("fs");
+const path = require("path");
 
 //render profile page
 module.exports.profile = (req, res) => {
@@ -10,12 +12,36 @@ module.exports.profile = (req, res) => {
   });
 };
 
-module.exports.update = (req, res) => {
+module.exports.update = async (req, res) => {
   if (req.user.id == req.params.id) {
-    User.findByIdAndUpdate(req.params.id, req.body, (err, user) => {
+    try {
+      let user = await User.findById(req.params.id);
+      User.uploadedAvatar(req, res, function (err) {
+        if (err) {
+          console.log("*****Multer Error:", err);
+        }
+        user.name = req.body.name;
+        user.email = req.body.email;
+        if (req.file) {
+          if (
+            user.avatar &&
+            fs.existsSync(path.join(__dirname, "..", user.avatar))
+          ) {
+            fs.unlinkSync(path.join(__dirname, "..", user.avatar));
+          }
+
+          //this is saving the path of the uploaded file into the avatar field in the user
+          user.avatar = User.avatarPath + "/" + req.file.filename;
+        }
+        user.save();
+        return res.redirect("back");
+      });
+    } catch (err) {
+      req.flash("error", err);
       return res.redirect("back");
-    });
+    }
   } else {
+    req.flash("error", Unauthorized);
     return res.status(401).send("Unauthorized");
   }
 };
